64d25770407abe7332705d8e2d32ab6e9fbc5874
Riyaa-k <anshitakoshta28@gmail.com>
2023-12-31 23:07:50 +0530

    song 1

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -3,16 +3,16 @@ import { useDispatch } from "react-redux";
 import PlayPause from "./PlayPause";
 import { playPause,setActiveSong } from "../redux/features/playerSlice";
 
-const SongCard = ({song}) => {
+const SongCard = ({song,isPlaying,activeSong,data}) => {
 
-  const activeSong="Test";
+  // const activeSong="Test";
 
 
   const handlePauseClick=()=>{
 
   }
   const handlePlayClick=()=>{
-    
+
   }
 
 return(
@@ -20,7 +20,9 @@ return(
     <div className="relative w-full h-56 group" >
       <div className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex
       ${activeSong?.title === song.title ? 'flex bg-black bg-opacity-70' : 'hidden'}`} >
-        <PlayPause 
+        <PlayPause
+        isPlaying={isPlaying}
+         activeSong={activeSong}
         song={song}
         handlePause={handlePauseClick}
         handlePlay={handlePlayClick}
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,9 +1,17 @@
+import { useDispatch,useSelector } from "react-redux";
 
 import {Error,Loader,SongCard} from "../components";
 import {genres} from "../assets/constants";
 import { useGetTopChartsQuery } from "../redux/Services/ShazamCore";
 
+CAKE={
+    CHOCO:MUSIC PLAYER FUNCTIONALITY
+    VANILLA:SHAZAM CORE FUNCTIONALITY
+}
+
 const Discover = () => {
+    const dispatch = useDispatch();
+    const { activeSong,isPlaying } = useSelector((state)=>state.player);  
     const {data,isFetching,error}=useGetTopChartsQuery();
     const genreTitle="Pop";
 
@@ -32,6 +40,9 @@ const Discover = () => {
                 {data.tracks?.map((song)=>(
                     <SongCard key={song.key} 
                     song={song}
+                    isPlaying={isPlaying}
+                    activeSong={activeSong}
+                    data={data}
                     
                    
                     />
